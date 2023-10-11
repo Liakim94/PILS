ad078e63028298497f2391682b32d1ad2eacd9b4
liakim94 <do942003@gmail.com>
2023-10-11 16:34:19 +0900

    23.10.11 - 이메일 발송 수정

--- src/main/java/kr/co/xicom/front/controller/QnaController.java
+++ src/main/java/kr/co/xicom/front/controller/QnaController.java
@@ -140,7 +140,7 @@ public class QnaController extends Alerts {
             if (result > 0) {
                 // 질의 등록 알림 메일 발송
                 try {
-                    EmsResponse ems = emsClient.send("do942003@u-cube.kr", "납품대금 연동제 클라우드 이메일 발송 테스트", "납품대금 연동제 클라우드 이메일 발송 테스트");
+                    EmsResponse ems = emsClient.send(qnaVO.getEmail(), "납품대금 연동제 클라우드 이메일 발송 테스트", "납품대금 연동제 클라우드 이메일 발송 테스트");
                     String status = ems.isSuccess() ? "SUCCESS" : "FAIL";
                     System.out.println(status);
                 }catch(Exception e){
